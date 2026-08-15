ad7ad220de92510c67c03d9524115aa83ed1bced
violet-vi <sahu.srishti08@gmail.com>
2026-08-16 00:37:37 +0530

    day 5

---
+++ c-codes/secs_converter_day_5,2.c
@@ -0,0 +1,12 @@
+// Q10 Write a program to input time in seconds and convert it into hours:minutes:seconds format
+
+# include<stdio.h>
+int main(){
+    int Sec_in , hrs ,mins ,sec;
+    scanf("%d", &Sec_in);
+    hrs = Sec_in / 3600;
+    mins = (Sec_in % 3600) / 60;
+    sec = (mins % 60);
+    printf("%d:%d:%d", hrs, mins, sec);
+    return 0;
+}
--- c-codes/si_ci_day_5,1.c
+++ c-codes/si_ci_day_5,1.c
@@ -1,4 +1,4 @@
-//Q9 Write a program to calculate simple and compound interest for given principal, rate and time.
+//  Q9 Write a program to calculate simple and compound interest for given principal, rate and time.
 
 #include<stdio.h>
 #include<math.h>
