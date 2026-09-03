011e61d98dae938ed2ae10a57c4680a9a80a9b49
violet-vi <sahu.srishti08@gmail.com>
2026-09-03 11:14:01 +0530

    day-25

---
+++ c-codes/day-25/day25-2.c
@@ -0,0 +1,27 @@
+/*
+NAME: SRISHTI SAHU
+BATCH: 54
+SAP ID: 590037979
+DAY: DAY 24  QUESTION-2
+
+PROBLEM STATEMENT: 
+Q50: Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+*/
+#include<stdio.h>
+int main(){
+    for (int i =5;i>=1;i--){
+        for (int k =5;k>=i;k--){
+            printf(" ");
+        }
+        for (int j = 1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
