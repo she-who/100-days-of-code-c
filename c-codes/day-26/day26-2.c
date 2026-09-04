282be59b96d353a3d87284aab28e0d2fb44f47e6
violet-vi <sahu.srishti08@gmail.com>
2026-09-04 17:00:39 +0530

    day 26

---
+++ c-codes/day-26/day26-2.c
@@ -0,0 +1,30 @@
+/*
+NAME: SRISHTI SAHU
+BATCH: 54
+SAP ID: 590037979
+DAY: DAY 25  QUESTION-1
+
+PROBLEM STATEMENT: 
+Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+
+*/
