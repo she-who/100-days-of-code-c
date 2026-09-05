f1171c2e4b12d619c68bfbf586fb18c2b558385b
violet-vi <sahu.srishti08@gmail.com>
2026-09-05 21:17:04 +0530

    day-27

--- c-codes/day-27/day27-2.c
+++ c-codes/day-27/day27-2.c
@@ -18,17 +18,26 @@ Q54: Write a program to print the following pattern:
 */
 #include<stdio.h>
 int main(){
-    for (int i=1;i<=10;i+=2){
-        for (int j=1;j<=i;j+=1){
-            printf("*");
-        }
-        printf("\n");
-    }
     for (int i=7;i>=1;i-=2){
-        for (int j=i;j>=1;j-=1){
-            printf("*");
+        for (int j=1;j<=i-j;j+=1){
+            printf("  ");
+        }
+        for (int k = 7;k>=i;k-=1){
+            printf(" *");
+        }
+        printf("\n");
+
+    }
+    
+    for (int i=1;i<=5;i+=2){
+        for (int j=i;j>=1;j-=2){
+            printf("  ");
+        }
+        for (int k=5;k>=i;k-=1){
+            printf(" *");
         }
         printf("\n");
     }
+    
     return 0;
 }
